11cd637dd864b64f622dafb262e0fc0440a2453b
sasham43 <sashasemail@gmail.com>
2017-12-06 23:13:42 -0600

    write to board

--- Modules/currentTrack.js
+++ Modules/currentTrack.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 var Spotify = require('spotify-web-api-node');
 var fs = require('fs');
 var path = require('path');
+var request = require('request');
+var host = '10.1.100.4'; // or maybe localhost
 
 var spotify = new Spotify({
     redirectUri: 'http://localhost:3009/callback',
@@ -10,12 +12,11 @@ var spotify = new Spotify({
     clientSecret: process.env.SPOTIFY_SECRET
 });
 
-// var refresh_token;
-
 var refresh_token = fs.readFileSync(path.join(__dirname, '../token.txt'), 'utf-8').trim();
-// var refresh_token = 'AQDtK-bRlhqdxVHIbp5CSM_z5K14_CXsOkLOKx0bxaTTAiisOlmYjW9_mQrei35TZr9XgRWdC4XPudMXkFTHY9QpYHvbJ2oUnojqY2tJVzB7rXppwEzZyaCM2JSob9gXWSw'
 var access_token;
 var tracks = [];
+var PADDING = '                                                              ';
+
 
 console.log('token:', refresh_token);
 
@@ -29,8 +30,6 @@ spotify.refreshAccessToken().then(function(response){
 }).catch(function(err){
     console.log('err:', err);
 });
-
-// getRecentlyPlayed();
 
 setInterval(getRecentlyPlayed, 10000);
 
@@ -47,6 +46,20 @@ function getRecentlyPlayed() {
         if(tracks.length > 3){
             tracks.pop();
         }
+
+        // write to board
+        tracks.forEach(function(item, index){
+            var prefix = getPrefix(index);
+            var text = `${prefix} ${item.artist} - ${item.track}${PADDING}`;
+            var line = index + 9;
+            var options = {
+                uri: `http://${host}/peggy/write?board=1&y=${line}&x=0&text=${text}`
+            }
+            request.get(options, function(err, response){
+                console.log(err || 'wrote track ' + index)
+            });
+        });
+
     }).catch(function(err){
         console.log('err:', err);
         if(err.statusCode == 401){
@@ -60,13 +73,21 @@ function refreshToken() {
         console.log('new token:', response.body.access_token);
         access_token = response.body.access_token;
         spotify.setAccessToken(access_token);
-
-        // getRecentlyPlayed()
     }).catch(function(err){
         console.log('err:', err);
     });
 }
 
+function getPrefix(index){
+    switch(index){
+        case 0:
+            return '{g}Now Playing: ';
+        case 1:
+            return '{r}Just Played: ';
+        case 2:
+            return '{g}Before That: ';
+    }
+}
 
 // options.path = '/peggy/write?board=1&x=1&y=9&text=' + encodeURIComponent(nowPlayingText);
 // options.path = '/peggy/write?board=1&x=1&y=10&text=' + encodeURIComponent(lastPlayedText);
